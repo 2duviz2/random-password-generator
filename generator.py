2e2539144ad5122216a5e1b292398099c2c33b2e
Duviz 2 <91156184+javierduviz2@users.noreply.github.com>
2023-04-20 20:17:28 +0200

    Python file

--- generator.py
+++ generator.py
@@ -3,7 +3,7 @@ import random
 print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
 a = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-times = input("Cuantos caracteres quieres? ")
+times = input("How many characters you want? ")
 alltext = "error!"
 error = False
 
@@ -13,7 +13,7 @@ if times.isnumeric() == False:
 
 if error == False:
     if int(times) > 9999:
-        aceptar = input("HEY HEY HEY, eso son muchos numeros, podria haber un problema si lo ejecutas, deseas ponerlo a 9999? (N/Y) ")
+        aceptar = input("HEY, that is a LOT of numbers, do you want to set it to 9999? (N/Y) ")
         if aceptar == "Y" or aceptar == "y":
             times = "9999"
     if int(times) > 0:
@@ -22,3 +22,4 @@ if error == False:
         randomItem = random.choice(a)
         alltext = alltext + randomItem
     print(alltext)
+pause = input("")
